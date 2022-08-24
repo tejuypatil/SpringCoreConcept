9f97428a7a8e3c005174207897cb6e5ce1bad741
tejuypatil <tejuypatil@gmail.com>
2022-08-24 19:46:32 +0530

    Created Project to Learn Spring Concepts

--- src/main/java/io/spring/start/mygroup/mySpringApp/SpringAppApplication.java
+++ src/main/java/io/spring/start/mygroup/mySpringApp/SpringAppApplication.java
@@ -2,13 +2,17 @@ package io.spring.start.mygroup.mySpringApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import io.spring.start.mygroup.mySpringApp.component.DemoBean;
 
 @SpringBootApplication
 public class SpringAppApplication {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Spring concept demo ");
-		SpringApplication.run(SpringAppApplication.class, args);
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Spring Concept Demo");
+		ApplicationContext context=SpringApplication.run(SpringAppApplication.class, args);
+		System.out.println("Checking Context:"+context.getBean(DemoBean.class));
 	}
 
 }
